ab39c6527f45e16c8672891492303ed80cbf1555
ngohongthong <baphongpine@gmail.com>
2023-08-11 23:50:38 +0700

    Weather UI

--- src/components/calendar/index.jsx
+++ src/components/calendar/index.jsx
@@ -1,4 +1,5 @@
 import classnames from 'classnames/bind';
+import  { useState } from 'react';
 import styles from './calendar.module.scss';
 
 const  cx = classnames.bind(styles);
@@ -6,9 +7,53 @@ const  cx = classnames.bind(styles);
 
 
 const Calendar = () => {
+
+    const [newMonth, setNewMonth] = useState(0);
+
+    const getDaysInMonth = (month, year) => {
+        return new Date(year, month, 0).getDate();
+    }
+    let dateActive = new Date();
+    let date = new Date();
+
+    let month = date.getMonth();
+    let year = date.getFullYear();
+    let active = date.getDate();
+
+    date = new Date(year, month + newMonth, active);
+
+    month = date.getMonth();
+    year = date.getFullYear();
+    active = date.getDate();
+    
+    const daysInMonth = getDaysInMonth(month, year);
+    const firstDayInMonth = new Date(year, month, 1).getDay();
+
+    function getMonthName(monthNumber) {
+        date.setMonth(monthNumber - 1);
+        return date.toLocaleString('en-US', { month: 'long' });
+    }
+
+    const calendar = []
+   
+    for(let i = 0; i < 35; i++){
+        if (i < firstDayInMonth || i > daysInMonth + firstDayInMonth - 1){
+            calendar.push(null)
+        }else{
+            calendar.push(i - firstDayInMonth + 1)
+        }
+    }
+   
+
+
     return (
         <div>
-            <h3 className={cx("title")}>August</h3>
+            <div className={cx("title-wrapper")}>
+                <span onClick={() => setNewMonth(newMonth - 1)}>{"<"}</span>
+                <h3>{getMonthName(month + 1)}</h3>
+                <p>{year}</p>
+                <span onClick={() => setNewMonth(newMonth + 1)}>{">"}</span>
+            </div>
             <table className={cx("custom-table","table-dark","table")}>
                 <thead>
                     <tr>
@@ -22,51 +67,25 @@ const Calendar = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td>4</td>
-                        <td>5</td>
-                        <td>6</td>
-                        <td>7</td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td>4</td>
-                        <td>5</td>
-                        <td>6</td>
-                        <td>7</td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td>4</td>
-                        <td>5</td>
-                        <td>6</td>
-                        <td>7</td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td>4</td>
-                        <td>5</td>
-                        <td>6</td>
-                        <td>7</td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td>4</td>
-                        <td>5</td>
-                        <td>6</td>
-                        <td>7</td>
-                    </tr>
+                    {
+                        calendar.map((item, index) => {
+                            if (index % 7 === 0){
+                                return (
+                                    <tr key={index}>
+                                        {
+                                            calendar.slice(index, index + 7).map((item, index) => {
+                                                return <td 
+                                                        key={index} 
+                                                        className={cx({"active": (item === active && month === dateActive.getMonth() && year === dateActive.getFullYear())})}
+                                                    >{item}
+                                                    </td>
+                                            })
+                                        }
+                                    </tr>
+                                )
+                            }
+                        })
+                    }
                 </tbody>
             </table>
         </div>
--- src/components/weather/index.jsx
+++ src/components/weather/index.jsx
@@ -1,5 +1,5 @@
 import classnames from 'classnames/bind';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from './weather.module.scss';
 
 const  cx = classnames.bind(styles);
@@ -8,37 +8,67 @@ const  cx = classnames.bind(styles);
 
 const Weather = () => {
     const [weather, setWeather] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    
-    // useEffect(() => {
-    //     const fetchWeather = async () => {
-    //     try {
-    //         const response = await axios.get(
-    //         `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
-    //         );
-    //         setWeather(response.data);
-    //         setLoading(false);
-    //     } catch (error) {
-    //         setError(error);
-    //         setLoading(false);
-    //     }
-    //     };
-    
-    //     fetchWeather();
-    // }, []);
-    
-    // if (loading) {
-    //     return <p>Loading...</p>;
-    // }
-    
-    // if (error) {
-    //     return <p>{error.message}</p>;
-    // }
-    
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/weather/')
+        .then(response => response.json())
+        .then(data => setWeather(data));
+    }, []);
     return (
-        <div>
-            <h1>Weather</h1>
+        <div className={cx("weather")}>
+            <div className={cx("weather-icon")}>
+                <h3 className={cx("title")}>{weather?.data?.main?.location}</h3>
+                <div className={cx("info-top")}>
+                    <div className = {cx("info-top-wrapper")}>
+                        <ul>
+                            <li>{weather?.data?.main?.status}</li>
+                            <li>Temp: {weather?.data?.main?.temp}</li>
+                            <li>{weather?.data?.main?.wind}</li>
+                            <li>{weather?.data?.main?.forecast}</li>
+                        </ul>
+                    </div>
+                    <img src={weather?.data?.main?.icon} alt="weather-icon"/>
+                </div>
+            </div>
+            <div className={cx("info-bottom")}>
+                <h3 className={cx("title")}>{weather?.data?.more?.title}</h3>
+                <table className={"table table-dark"}>
+                    <thead>
+                        <tr>
+                            <th>{weather?.data?.more?.data[0][0]}</th>
+                            <th>{weather?.data?.more?.data[0][1]}</th>
+                            <th>{weather?.data?.more?.data[0][2]}</th>
+                            <th>{weather?.data?.more?.data[0][3]}</th>
+                            <th>{weather?.data?.more?.data[0][4]}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>
+                                <img src={weather?.data?.more?.data[1][0]}></img>
+                            </th>
+                            <th>
+                                <img src={weather?.data?.more?.data[1][1]}></img>
+                            </th>
+                            <th>
+                                <img src={weather?.data?.more?.data[1][2]}></img>
+                            </th>
+                            <th>
+                                <img src={weather?.data?.more?.data[1][3]}></img>
+                            </th>
+                            <th>
+                                <img src={weather?.data?.more?.data[1][4]}></img>
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>{weather?.data?.more?.data[2][0]}</th>
+                            <th>{weather?.data?.more?.data[2][1]}</th>
+                            <th>{weather?.data?.more?.data[2][2]}</th>
+                            <th>{weather?.data?.more?.data[2][3]}</th>
+                            <th>{weather?.data?.more?.data[2][4]}</th>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
